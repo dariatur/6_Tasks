f7dcf3ff6d6f336807f044209c9d7267604a0b5f
Daria <97191466+DariaTur@users.noreply.github.com>
2022-05-23 23:05:31 +0300

    Update Main.java

--- module_6/home_library/src/by/epam/training/module6/main/Main.java
+++ module_6/home_library/src/by/epam/training/module6/main/Main.java
@@ -6,11 +6,6 @@ public class Main {
 
 	
 	public static void main(String[] args){
-
-//		User user1 = new User("Dasha","Tur", "darida","sanddki12","dasha@mail.ru",UserType.ADMIN);
-//		User user2 = new User("Dasha","Tur", "fada","sa","dasha@mail.ru",UserType.USER);
-//		turbina computer8761
-
 		Menu menu = Menu.getInstance();
 		menu.startMenu();
 	}
