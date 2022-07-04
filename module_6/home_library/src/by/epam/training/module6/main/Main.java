cbda466640b01ccf0002677216d4fd76b5d0de83
Daria <97191466+DariaTur@users.noreply.github.com>
2022-07-04 10:24:41 +0300

    Update Main.java

--- module_6/home_library/src/by/epam/training/module6/main/Main.java
+++ module_6/home_library/src/by/epam/training/module6/main/Main.java
@@ -3,7 +3,6 @@ package by.epam.training.module6.main;
 import by.epam.training.module6.menu.Menu;
 
 public class Main {
-
 	
 	public static void main(String[] args){
 		Menu menu = Menu.getInstance();
